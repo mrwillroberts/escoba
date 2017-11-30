d34b3bc6e5cc9f692bfbe4a040ba8c281f0cf45a
Will Roberts <4405078+mrwillroberts@users.noreply.github.com>
2017-11-30 22:00:08 +0000

    + Render current state in jquery + Add dev tools for quicker dev (required switching to jdk 1.8 due to not 9 compatible)

--- src/main/resources/static/escoba_cards.js
+++ src/main/resources/static/escoba_cards.js
@@ -1,7 +1,7 @@
 function position(deck,cards,side,y_offset) {
     var i = 0;
-    for (var index in cards) {
-        var card = deck.cards[index];
+    jQuery.each(cards, function() {
+        var card = deck.cards[this];
         //card.enableDragging();
         card.setSide(side);
         card.mount($container);
@@ -18,12 +18,36 @@ function position(deck,cards,side,y_offset) {
 //            card.$el.style["border-style"] = "solid";
 //        });
         i++;
+    });
+}
+
+function lookupSuitMultiplier(suit){
+    switch(suit) {
+        case "Coin":
+            return 0;
+        case "Sword":
+            return 1;
+        case "Cup":
+            return 2;
+        case "Club":
+            return 3;
     }
+}
+
+function lookupCardIndices(cardArray) {
+    var guiCardsIndices = []
+    jQuery.each(cardArray, function() {
+        guiCardsIndices.push(lookupSuitMultiplier(this.suit)*13+this.value);
+    });
+    return guiCardsIndices;
 }
 
 function setup($container){
 
-    position(Deck(),[0,1,2], 'front',200);
-    position(Deck(),[3,4,5,6], 'front',0);
-    position(Deck(),[7,8,9], 'back',-200);
+    $.get("/api/game/1", function(data, status){
+        position(Deck(),[7,8,9], 'back',-200);
+        position(Deck(),lookupCardIndices(data.currentPlayerHand), 'front',200);
+        position(Deck(),lookupCardIndices(data.tableCards), 'front',0);
+    });
+
 }
